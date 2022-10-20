86d93e79e19a6e5c3821686a4365e385d69bf7fa
Kamran <311.kamran@gmail.com>
2022-10-20 05:14:20 +0400

    Add project files.

---
+++ WordPad_Killer/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace WordPad_Killer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_font = new System.Windows.Forms.Label();
+            this.label_size = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbox_font = new System.Windows.Forms.ComboBox();
+            this.cbox_size = new System.Windows.Forms.ComboBox();
+            this.btn_bold = new System.Windows.Forms.Button();
+            this.btn_underLine = new System.Windows.Forms.Button();
+            this.btn_italic = new System.Windows.Forms.Button();
+            this.label_alignment = new System.Windows.Forms.Label();
+            this.btn_left = new System.Windows.Forms.Button();
+            this.btn_center = new System.Windows.Forms.Button();
+            this.btn_right = new System.Windows.Forms.Button();
+            this.label_Color = new System.Windows.Forms.Label();
+            this.cbox_color = new System.Windows.Forms.ComboBox();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label_font
+            // 
+            this.label_font.AutoSize = true;
+            this.label_font.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label_font.Location = new System.Drawing.Point(35, 9);
+            this.label_font.Name = "label_font";
+            this.label_font.Size = new System.Drawing.Size(48, 23);
+            this.label_font.TabIndex = 0;
+            this.label_font.Text = "Font";
+            // 
+            // label_size
+            // 
+            this.label_size.AutoSize = true;
+            this.label_size.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label_size.Location = new System.Drawing.Point(149, 9);
+            this.label_size.Name = "label_size";
+            this.label_size.Size = new System.Drawing.Size(46, 23);
+            this.label_size.TabIndex = 0;
+            this.label_size.Text = "Size";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(250, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "FontStyle";
+            // 
+            // cbox_font
+            // 
+            this.cbox_font.FormattingEnabled = true;
+            this.cbox_font.Location = new System.Drawing.Point(10, 35);
+            this.cbox_font.Name = "cbox_font";
+            this.cbox_font.Size = new System.Drawing.Size(94, 28);
+            this.cbox_font.TabIndex = 1;
+            // 
+            // cbox_size
+            // 
+            this.cbox_size.FormattingEnabled = true;
+            this.cbox_size.Location = new System.Drawing.Point(125, 35);
+            this.cbox_size.Name = "cbox_size";
+            this.cbox_size.Size = new System.Drawing.Size(94, 28);
+            this.cbox_size.TabIndex = 1;
+            // 
+            // btn_bold
+            // 
+            this.btn_bold.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_bold.Location = new System.Drawing.Point(247, 35);
+            this.btn_bold.Name = "btn_bold";
+            this.btn_bold.Size = new System.Drawing.Size(27, 29);
+            this.btn_bold.TabIndex = 2;
+            this.btn_bold.Text = "B";
+            this.btn_bold.UseVisualStyleBackColor = true;
+            // 
+            // btn_underLine
+            // 
+            this.btn_underLine.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_underLine.Location = new System.Drawing.Point(280, 35);
+            this.btn_underLine.Name = "btn_underLine";
+            this.btn_underLine.Size = new System.Drawing.Size(27, 29);
+            this.btn_underLine.TabIndex = 2;
+            this.btn_underLine.Text = "U";
+            this.btn_underLine.UseVisualStyleBackColor = true;
+            // 
+            // btn_italic
+            // 
+            this.btn_italic.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btn_italic.Location = new System.Drawing.Point(313, 35);
+            this.btn_italic.Name = "btn_italic";
+            this.btn_italic.Size = new System.Drawing.Size(27, 29);
+            this.btn_italic.TabIndex = 2;
+            this.btn_italic.Text = "I";
+            this.btn_italic.UseVisualStyleBackColor = true;
+            // 
+            // label_alignment
+            // 
+            this.label_alignment.AutoSize = true;
+            this.label_alignment.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label_alignment.Location = new System.Drawing.Point(361, 9);
+            this.label_alignment.Name = "label_alignment";
+            this.label_alignment.Size = new System.Drawing.Size(96, 23);
+            this.label_alignment.TabIndex = 0;
+            this.label_alignment.Text = "Alignment";
+            // 
+            // btn_left
+            // 
+            this.btn_left.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_left.Location = new System.Drawing.Point(361, 35);
+            this.btn_left.Name = "btn_left";
+            this.btn_left.Size = new System.Drawing.Size(27, 29);
+            this.btn_left.TabIndex = 2;
+            this.btn_left.Text = "L";
+            this.btn_left.UseVisualStyleBackColor = true;
+            // 
+            // btn_center
+            // 
+            this.btn_center.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_center.Location = new System.Drawing.Point(394, 35);
+            this.btn_center.Name = "btn_center";
+            this.btn_center.Size = new System.Drawing.Size(27, 29);
+            this.btn_center.TabIndex = 2;
+            this.btn_center.Text = "C";
+            this.btn_center.UseVisualStyleBackColor = true;
+            // 
+            // btn_right
+            // 
+            this.btn_right.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_right.Location = new System.Drawing.Point(427, 35);
+            this.btn_right.Name = "btn_right";
+            this.btn_right.Size = new System.Drawing.Size(27, 29);
+            this.btn_right.TabIndex = 2;
+            this.btn_right.Text = "R";
+            this.btn_right.UseVisualStyleBackColor = true;
+            // 
+            // label_Color
+            // 
+            this.label_Color.AutoSize = true;
+            this.label_Color.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label_Color.Location = new System.Drawing.Point(504, 9);
+            this.label_Color.Name = "label_Color";
+            this.label_Color.Size = new System.Drawing.Size(57, 23);
+            this.label_Color.TabIndex = 0;
+            this.label_Color.Text = "Color";
+            // 
+            // cbox_color
+            // 
+            this.cbox_color.FormattingEnabled = true;
+            this.cbox_color.Location = new System.Drawing.Point(484, 35);
+            this.cbox_color.Name = "cbox_color";
+            this.cbox_color.Size = new System.Drawing.Size(94, 28);
+            this.cbox_color.TabIndex = 1;
+            // 
+            // btn_load
+            // 
+            this.btn_load.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_load.Location = new System.Drawing.Point(604, 35);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(63, 29);
+            this.btn_load.TabIndex = 2;
+            this.btn_load.Text = "Load";
+            this.btn_load.UseVisualStyleBackColor = true;
+            // 
+            // btn_open
+            // 
+            this.btn_open.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_open.Location = new System.Drawing.Point(683, 35);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(86, 29);
+            this.btn_open.TabIndex = 2;
+            this.btn_open.Text = "Open";
+            this.btn_open.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 79);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(756, 359);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_right);
+            this.Controls.Add(this.btn_center);
+            this.Controls.Add(this.btn_left);
+            this.Controls.Add(this.btn_italic);
+            this.Controls.Add(this.btn_underLine);
+            this.Controls.Add(this.btn_open);
+            this.Controls.Add(this.btn_load);
+            this.Controls.Add(this.btn_bold);
+            this.Controls.Add(this.cbox_color);
+            this.Controls.Add(this.cbox_size);
+            this.Controls.Add(this.cbox_font);
+            this.Controls.Add(this.label_Color);
+            this.Controls.Add(this.label_alignment);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label_size);
+            this.Controls.Add(this.label_font);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label_font;
+        private Label label_size;
+        private Label label2;
+        private ComboBox cbox_font;
+        private ComboBox cbox_size;
+        private Button btn_bold;
+        private Button btn_underLine;
+        private Button btn_italic;
+        private Label label_alignment;
+        private Button btn_left;
+        private Button btn_center;
+        private Button btn_right;
+        private Label label_Color;
+        private ComboBox cbox_color;
+        private Button btn_load;
+        private Button btn_open;
+        private TextBox textBox1;
+    }
+}
---
+++ WordPad_Killer/Form1.cs
@@ -0,0 +1,10 @@
+namespace WordPad_Killer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
